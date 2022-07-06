9c469b2b888e3517657cdb3fc5a30a14a0bbd944
igor bastos <igorbastoscic@gmail.com>
2022-07-06 19:49:06 -0300

    MainProgram adicionado

--- src/model/service/RentalServise.java
+++ src/model/service/RentalServise.java
@@ -1,6 +1,7 @@
 package model.service;
 
 import model.entities.CarRental;
+import model.entities.Invoice;
 
 public class RentalServise {
     private Double pricePerDay;
@@ -16,6 +17,19 @@ public class RentalServise {
     }
 
     public void processInvoice(CarRental carRental){
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+        double hours = (double) (t2 - t1) / 1000 / 60 / 60;
+        double basicPayment;
+        if(hours <= 12.0){
+            basicPayment = Math.ceil(hours) * pricePerHour;
+        } else {
+            basicPayment = Math.ceil(hours/24) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
 
     }
 }
